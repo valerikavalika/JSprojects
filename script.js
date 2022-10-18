14e4f02a5fb89cc3a41ecfd0eae1bcff57598e5d
Jurigova Valeriya <valeriyajurigova@gmail.com>
2022-10-18 19:28:29 +0200

    main done

--- script.js
+++ script.js
@@ -1,20 +1,93 @@
-//colors array
-let colors = ['aquamarine', 'lightpink', 'salmon', 'orange', 'yellow', 'rgba(133,122,200)', '#F15025']; 
+//items 
+const menu = [
+    {
+        id: 1,
+        title: "Buttermilk pancakes",
+        category: "Breakfast",
+        price: 15.99,
+        img: "/img/serving.jpg",
+        desc: 'Lovely buttermilk pancakes',
+    },
+    {
+        id: 2,
+        title: "Dinner double",
+        category: "Lunch",
+        price: 13.99,
+        img: "/img/serving.jpg",
+        desc: 'Awesome dinner double',
+    },
+    {
+        id: 3,
+        title: "Oreo dream",
+        category: "Shakes",
+        price: 18.99,
+        img: "/img/serving.jpg",
+        desc: 'OREOOO',
+    },
+    {
+        id: 4,
+        title: "Steak dinner",
+        category: "Dinner",
+        price: 39.99,
+        img: "/img/serving.jpg",
+        desc: 'Yummy meat',
+    },
+];
 
-// get button, title and container from html
-let button = document.getElementById('btn');
-let container = document.getElementById('container');
-let title =document.getElementById('title');
+ const sectionCenter = document.querySelector('.section-center');
+ const containerBtn = document.querySelector('.btn-container');
+ 
 
-// set default title value
-title.innerText = `Background color is: ${colors[0]}`;
-//set default container background color 
-container.style.backgroundColor = colors[0];
+ window.addEventListener('DOMContentLoaded', function () {
+    displayMenuItems(menu);
+    displayMenuBtns();
+    });
 
-//add eventListener
-button.addEventListener('click', changeBackground);
-function changeBackground() {
-    let colorsIndex = Math.floor(Math.random()*colors.length);
-    container.style.backgroundColor = colors[colorsIndex];
-    title.innerText = `Background color is: ${colors[colorsIndex]}`;
-}
+ function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function(item) {
+        return `<article class="menu-item item">
+        <img  class="item__img" src=${item.img} alt=${item.title}>
+        <div class="item__info">
+            <header class="item__header">
+                <h4 class="item__title">${item.title}</h4>
+                <h4 class="item__price">$ ${item.price}</h4>
+            </header>
+            <div class="item__underline"></div>
+            <p class="item__text">${item.desc}</p>
+        </div>
+    </article>`;
+    });
+    displayMenu = displayMenu.join("");
+    sectionCenter.innerHTML = displayMenu;
+ };
+
+ function displayMenuBtns () {
+    const categories = menu.reduce(function(values,item){
+        if(!values.includes(item.category)) {
+            values.push(item.category);
+        }
+        return values;
+    },['All']);
+    const categoryBtns = categories.map(function(category) {
+        return `<button class="filter-btn" type="button" data-category="${category}">${category}</button>`
+    }).join('');
+    containerBtn.innerHTML = categoryBtns;
+    //filter
+    const filterBtns = document.querySelectorAll('.filter-btn');
+    filterBtns.forEach(function(btn) {
+        btn.addEventListener('click', function(e) {
+            const category = e.currentTarget.dataset.category;
+            const menuCategory = menu.filter(function(menuItem){
+                if(menuItem.category === category) {
+                    return menuItem;
+                }
+            });
+            if(category === "All") {
+                displayMenuItems(menu);
+            }
+            else {
+                displayMenuItems(menuCategory);
+            }
+        });
+    });
+ };
