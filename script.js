a3c98b005283c816a296920bbe7e529bf32d96da
Jurigova Valeriya <valeriyajurigova@gmail.com>
2022-10-24 10:46:12 +0200

    selection sort added

--- script.js
+++ script.js
@@ -17,5 +17,24 @@ const bubbleSort = arr => {
     }
     return arr;
 };
+console.log (bubbleSort(arr));
 
 //Selection sort
+const arr2 =  [8, 12, 1, 5, 2]; 
+const selectionSort = arr => {
+    for (let i = 0; i < arr.length; i++){
+        let lowest = i;
+        for (let j = i + 1; j < arr.length;j++) {
+            if(arr[j] < arr[lowest]){
+            lowest = j;
+            }
+        }
+    if (lowest !== i) {
+        [arr[i], arr[lowest]] = [arr[lowest], arr[i]]
+        }
+    }
+    return arr;
+}
+console.log (selectionSort(arr2));
+
+//
