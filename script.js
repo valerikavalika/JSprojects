0b0e882de73e4e6b11596a57b29cfaf1f2fc14a2
Jurigova Valeriya <valeriyajurigova@gmail.com>
2022-10-24 13:12:13 +0200

    quickSort added

--- script.js
+++ script.js
@@ -37,4 +37,48 @@ const selectionSort = arr => {
 }
 console.log (selectionSort(arr2));
 
+//Insertion sort 
+const arr3 = [5, 2, 4, 6, 1, 3];
+const insertionSort = arr => {
+    for(let i = 1; i < arr.length; i++) {
+        let currentValue = arr[i];
+        let j;
+        for(j = i - 1; j >=0 && arr[j] > currentValue; j--) {
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = currentValue;
+    }
+    return arr;
+}
+console.log (insertionSort(arr3));
+
+//quickSort 
+const arr4 = [3, 2, 4, 9, 1, 0, 8, 7];
+function partition (arr,start,end) {
+    const pivotValue = arr[start];
+    let swapIndex = start;
+    for(let i = start + 1; i <= end; i++){
+        if(pivotValue > arr[i]) {
+            swapIndex++;
+            if(i !== swapIndex) {
+                [arr[i], arr[swapIndex]] = [arr[swapIndex], arr[i]];
+            };
+        };  
+    };
+    if(swapIndex !== start) {
+        [arr[swapIndex], arr[start]] = [arr[start], arr[swapIndex]];
+    };
+    return swapIndex;
+}
+const quickSort = (arr, start = 0, end = arr.length - 1) => {
+    if(start >= end) {
+        return;
+    }
+    let pivotIndex = partition(arr,start,end);
+    quickSort(arr,start,pivotIndex-1);
+    quickSort(arr, pivotIndex + 1,end);
+    return arr;
+}
+console.log(quickSort(arr4));
+
 //
