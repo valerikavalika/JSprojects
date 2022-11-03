f437b4c09ea8614b1a78b4bad7a7cdc21831bcbd
Jurigova Valeriya <valeriyajurigova@gmail.com>
2022-11-03 20:41:58 +0100

    done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const formElement = document.querySelector('.form');
 const formInputElement = document.querySelector('.form__input');
+const submitButton = document.querySelector('.form__submit');
 const tasksList = document.querySelector('.list__tasks');
 
 window.addEventListener('load', () =>{
@@ -9,137 +10,167 @@ window.addEventListener('load', () =>{
     } else {
         tasksList.style.visibility = "visible";
     displayTasks(cookies);
-    console.log(cookies);
     }
 });
 
-formElement.addEventListener('submit', (e) => {
+submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     tasksList.style.visibility = "visible";
     createNewTask();
 });
+formElement.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    if(e.key == "Enter") {
+        e.preventDefault();
+        tasksList.style.visibility = "visible";
+        createNewTask();
+    }
+});
 
 function createNewTask () {
     const task = formInputElement.value;
+    console.log(task);
     if(task == "") {
-        console.log("empty input")
+        return;
     } else {
-    const taskElement = document.createElement("div");
-    taskElement.classList.add("lisk__task");
-    taskElement.classList.add("task");
-    taskElement.setAttribute("id", String(task));
-    tasksList.appendChild(taskElement);
-
-    const taskContentElement = document.createElement("div");
-    taskContentElement.classList.add("task__content");
-    taskElement.appendChild(taskContentElement);
-
-    const taskActionsElement = document.createElement("div");
-    taskActionsElement.classList.add("task__action");
-    taskActionsElement.classList.add("action");
-    taskElement.appendChild(taskActionsElement);
-
-    const taskTextElement = document.createElement("input");
-    taskTextElement.classList.add("task__text");
-    taskTextElement.type = "text";
-    taskTextElement.value = task;
-    taskTextElement.setAttribute("readonly", "readonly")
-    taskContentElement.appendChild(taskTextElement);
-
-    const editButtonElement = document.createElement("button");
-    editButtonElement.classList.add("action__edit");
-    editButtonElement.classList.add("action__button");
-    editButtonElement.innerHTML = "Edit";
-    taskActionsElement.appendChild(editButtonElement);
-
-    const deleteButtonElement = document.createElement("button");
-    deleteButtonElement.classList.add("action__delete");
-    deleteButtonElement.classList.add("action__button");
-    deleteButtonElement.innerHTML = "Delete";
-    taskActionsElement.appendChild(deleteButtonElement);
+    const taskElement =  createTaskElement(task);
+    const taskContentElement =  createTaskContentElement(taskElement);
+    const taskActionsElement =  createTaskActionsElement(taskElement);
+    const taskTextElement =  createTaskTextElement(task, taskContentElement);
+    const editButtonElement =  createEditButtonElement(taskActionsElement);
+    const deleteButtonElement=  createDeleteButtonElement(taskActionsElement);
 
     const key = taskElement.id;
     document.cookie = `${key}=${task}; expires=` + new Date (2025, 10, 3).toUTCString();
+      
+    addEditEventListener(editButtonElement, taskTextElement, key, taskElement);
+    addEnterEventListener(editButtonElement, taskTextElement, key, taskElement)
+    addDeleteEventListener(deleteButtonElement, taskElement, key);
     formInputElement.value = "";
-    
-
-    editButtonElement.addEventListener('click', () => {
-        if(editButtonElement.innerText.toLowerCase() == "edit") {
-            taskTextElement.removeAttribute("readonly");
-            taskTextElement.focus();
-            editButtonElement.innerText = "Save";
-        } else {
-            taskTextElement.setAttribute("readonly", "readonly");
-            editButtonElement.innerText = "Edit";
-            const newValue = taskTextElement.value;
-            document.cookie = `${key}=${newValue}`;
-
-        }
-    });
-    
-    deleteButtonElement.addEventListener('click', () => {
-        tasksList.removeChild(taskElement);
-        document.cookie = `${key}=${taskElement.value}; expires=` + new Date(2000,0,1).toUTCString();
-    });
     }
 };
 function displayTasks (cookies) {
     let displayTasks = cookies.map((cookie) => {
     const cookieKey = `${cookie[0]}`;
     const cookieValue = `${cookie[1]}`;
+    const taskElement =  createTaskElement(cookieKey);
+    const taskContentElement =  createTaskContentElement(taskElement);
+    const taskActionsElement =  createTaskActionsElement(taskElement);
+    const taskTextElement =  createTaskTextElement(cookieValue, taskContentElement);
+    const editButtonElement =  createEditButtonElement(taskActionsElement);
+    const deleteButtonElement=  createDeleteButtonElement(taskActionsElement);
     
+    addEditEventListener(editButtonElement, taskTextElement, cookieKey, taskElement);
+    addEnterEventListener(editButtonElement, taskTextElement, cookieKey, taskElement)
+    addDeleteEventListener(deleteButtonElement, taskElement, cookieKey);
+    }
+    );
+};
+
+function createTaskElement(value) {
     const taskElement = document.createElement("div");
     taskElement.classList.add("lisk__task");
     taskElement.classList.add("task");
-    taskElement.setAttribute("id", `${cookieKey}`);
+    taskElement.setAttribute("id", String(value));
     tasksList.appendChild(taskElement);
-
+    return taskElement;
+};
+function createTaskContentElement(taskElement) {
     const taskContentElement = document.createElement("div");
     taskContentElement.classList.add("task__content");
     taskElement.appendChild(taskContentElement);
-
+    return taskContentElement;
+};
+function createTaskActionsElement(taskElement) {
     const taskActionsElement = document.createElement("div");
     taskActionsElement.classList.add("task__action");
     taskActionsElement.classList.add("action");
     taskElement.appendChild(taskActionsElement);
-
+    return taskActionsElement;
+};
+function createTaskTextElement(value, taskContentElement) {
     const taskTextElement = document.createElement("input");
     taskTextElement.classList.add("task__text");
-    taskTextElement.type = "text";
-    taskTextElement.value = `${cookieValue}`;
+    taskTextElement.type = "textarea";
+    taskTextElement.value = value;
     taskTextElement.setAttribute("readonly", "readonly")
     taskContentElement.appendChild(taskTextElement);
-
+    return taskTextElement;
+};
+// function createTaskTextElement(value, taskContentElement) {
+//     const taskTextElement = document.createElement("textarea");
+//     taskTextElement.classList.add("task__text");
+//     taskTextElement.value = value;
+//     taskTextElement.setAttribute("readonly", "readonly")
+//     taskTextElement.setAttribute("rows", "1")
+//     taskContentElement.appendChild(taskTextElement);
+//     return taskTextElement;
+// };
+function createEditButtonElement(taskActionsElement) {
     const editButtonElement = document.createElement("button");
     editButtonElement.classList.add("action__edit");
     editButtonElement.classList.add("action__button");
     editButtonElement.innerHTML = "Edit";
     taskActionsElement.appendChild(editButtonElement);
-
+    return editButtonElement;
+};
+function createDeleteButtonElement(taskActionsElement) {
     const deleteButtonElement = document.createElement("button");
     deleteButtonElement.classList.add("action__delete");
     deleteButtonElement.classList.add("action__button");
     deleteButtonElement.innerHTML = "Delete";
     taskActionsElement.appendChild(deleteButtonElement);
-    
+    return deleteButtonElement;
+};
 
+function addEditEventListener(editButtonElement, taskTextElement, cookieKey, taskElement) {
     editButtonElement.addEventListener('click', () => {
         if(editButtonElement.innerText.toLowerCase() == "edit") {
             taskTextElement.removeAttribute("readonly");
             taskTextElement.focus();
             editButtonElement.innerText = "Save";
         } else {
-            taskTextElement.setAttribute("readonly", "readonly");
-            editButtonElement.innerText = "Edit";
-            const newValue = taskTextElement.value;
-            document.cookie = `${cookieKey}=${newValue}`;
+            if(taskTextElement.value == "") {
+                const deleteChild = document.getElementById(`${cookieKey}`);
+                tasksList.removeChild(deleteChild);
+                document.cookie = `${cookieKey}=${taskTextElement.value}; expires=` + new Date(2000,0,1).toUTCString();
+            } else {
+                taskTextElement.setAttribute("readonly", "readonly");
+                editButtonElement.innerText = "Edit";
+                const newValue = taskTextElement.value;
+                document.cookie = `${cookieKey}=${newValue}`;
+            }
         }
     });
-    
+};
+function addEnterEventListener(editButtonElement, taskTextElement, cookieKey, taskElement) {
+    taskTextElement.addEventListener('keyup', (e) => {
+        e.preventDefault();
+        if(e.key == "Enter") {
+            e.preventDefault();
+            if(editButtonElement.innerText.toLowerCase() == "edit") {
+                taskTextElement.removeAttribute("readonly");
+                taskTextElement.focus();
+                editButtonElement.innerText = "Save";
+            } else {
+                if(taskTextElement.value == "") {
+                    const deleteChild = document.getElementById(`${cookieKey}`);
+                    tasksList.removeChild(deleteChild);
+                    document.cookie = `${cookieKey}=${taskTextElement.value}; expires=` + new Date(2000,0,1).toUTCString();
+                } else {
+                    taskTextElement.setAttribute("readonly", "readonly");
+                    editButtonElement.innerText = "Edit";
+                    const newValue = taskTextElement.value;
+                    document.cookie = `${cookieKey}=${newValue}`;
+                }
+            }
+        }
+    });
+};
+
+function addDeleteEventListener(deleteButtonElement, taskElement, cookieKey) {
     deleteButtonElement.addEventListener('click', () => {
         tasksList.removeChild(taskElement);
         document.cookie = `${cookieKey}=${taskElement.value}; expires=` + new Date(2000,0,1).toUTCString();
     });
-    }
-    );
 };
